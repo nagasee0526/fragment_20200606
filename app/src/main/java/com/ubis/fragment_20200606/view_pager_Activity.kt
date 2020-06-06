cb73c1db520e8695c8d2590f07c63f1ba31970b3
nagasee0526 <roronalove@gmail.com>
2020-06-06 12:33:25 +0900

    뷰페이제액티비티를 시작점으로

---
+++ app/src/main/java/com/ubis/fragment_20200606/view_pager_Activity.kt
@@ -0,0 +1,22 @@
+package com.ubis.fragment_20200606
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class view_pager_Activity :baseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager_)
+
+        setEvents()
+        setValues()
+    }
+
+    override fun setValues() {
+    }
+
+    override fun setEvents() {
+    }
+
+}

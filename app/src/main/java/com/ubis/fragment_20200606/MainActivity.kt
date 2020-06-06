6f29ab3fad248b620ef48e11d83adbc37eccbaac
nagasee0526 <roronalove@gmail.com>
2020-06-06 11:18:56 +0900

    Initial commit

---
+++ app/src/main/java/com/ubis/fragment_20200606/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.ubis.fragment_20200606
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : baseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setValues()
+        SetEvents()
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+
+    override fun SetEvents() {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ app/src/main/java/com/ubis/fragment_20200606/baseActivity.kt
@@ -0,0 +1,11 @@
+package com.ubis.fragment_20200606
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract  class baseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setValues()
+    abstract fun SetEvents()
+}
